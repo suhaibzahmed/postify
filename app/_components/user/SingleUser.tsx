45c2cf8d3107bb23a2f8e2ad6579c6f226efc425
suhaibzahmed <suhaib.z.ahmed@gmail.com>
2025-02-25 12:59:26 +0530

    Styled user cards

--- app/_components/user/SingleUser.tsx
+++ app/_components/user/SingleUser.tsx
@@ -11,9 +11,17 @@ async function SingleUser(props: iSingleUser) {
   const { user } = props
   const followingUser = await checkCurrentUserFollowing(user.clerkId)
 
+  function shortenUsername() {
+    const newUsername = user.username
+    if (user.username.length > 10) {
+      return newUsername.slice(0, 10).concat('...')
+    }
+    return newUsername
+  }
+
   return (
-    <div className="flex items-center justify-between border rounded-md h-24">
-      <div>
+    <div className="flex items-center justify-between border rounded-md h-[90px] px-6 py-4">
+      <div className="flex gap-x-2">
         <div className="h-8 w-8 rounded-full relative">
           {user.profileImage ? (
             <Image
@@ -30,7 +38,7 @@ async function SingleUser(props: iSingleUser) {
           <p>
             {user.firstName} {user.lastName}
           </p>
-          <p>@{user.username}</p>
+          <p className="text-gray-400">@{shortenUsername()}</p>
         </div>
       </div>
       <FollowButton
